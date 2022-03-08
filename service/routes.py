d2fe114675c417bccb1ea4b43c6c17b2c0655446
FrankCCCCCC <604996175@qq.com>
2022-03-08 01:36:28 -0500

    merged issue12

--- service/routes.py
+++ service/routes.py
@@ -9,6 +9,7 @@ import sys
 import logging
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from . import status  # HTTP Status Codes
+from werkzeug.exceptions import NotFound
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
@@ -24,8 +25,14 @@ from . import app
 @app.route("/")
 def index():
     """ Root URL response """
+    app.logger.info("Request for Root URL")
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Inventory REST API Service",
+            version="1.0",
+            list_path=url_for("list_products", _external=True),
+            # update_path=url_for("update_product", _external=False),
+        ),
         status.HTTP_200_OK,
     )
 
@@ -41,8 +48,43 @@ def init_db():
     Product.init_db(app)
     print("init database sucessfully")
 
+
+######################################################################
+# LIST ALL PRODUCTS
+######################################################################
+@app.route("/products", methods=["GET"])
+def list_products():
+    """Returns all of the Pets"""
+    app.logger.info("Request for product list")
+    products = []
+    products = Product.all()
+    results = [product.serialize() for product in products]
+    app.logger.info("Returning %d products", len(results))
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+
+######################################################################
+# RETRIEVE A PRODUCT
+######################################################################
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_products(product_id):
+    """
+    Retrieve a single Product
+    This endpoint will return a Product based on it's id
+    """
+    app.logger.info("Request for product with id: %s", product_id)
+    product = Product.find_or_404(product_id)
+    if not product:
+        raise NotFound("Product with id '{}' was not found.".format(product_id))
+    app.logger.info("Returning pet: %s", product.product_name)
+    return make_response(jsonify(product.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+# UPDATE AN EXISTING PRODUCT
+######################################################################
 @app.route('/products/<int:product_id>', methods=['PUT'])
-def update_product(product_id):
+def update_products(product_id):
     """ Update the quantity of product """
     app.logger.info('Request to update Product with id: {} by {}'.format(product_id, quantity))
 
