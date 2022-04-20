9a6b0f119ad8e6d88f7be518086ccfd037299e02
Zhaoxi Shi <jr028740@gmail.com>
2022-04-20 18:18:04 +0000

    Deployed to the cloud

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,7 +15,7 @@ from werkzeug.exceptions import NotFound
 from .factories import ProductFactory
 
 DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgresql://postgres:postgres@postgres:5432/testdb"
+    "TEST_DATABASE_URI", "postgresql://postgres:postgres@postgres:5432/testdb"
 )
 
 ######################################################################
